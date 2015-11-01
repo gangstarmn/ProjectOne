16fca746a1728d67dbe456bb8ad3360ab42b898a
gangstarmn <gantulgats@Gantulgas-iMac.local>
2015-11-01 11:44:43 +0800

    delete images

--- ProjectOne/ViewController.swift
+++ ProjectOne/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var toolbar = UIToolbar(frame: CGRectMake(0,0,self.view.bounds.width,self.view.bounds.height));
+        self.view.addSubview(toolbar);
         // Do any additional setup after loading the view, typically from a nib.
     }
 
